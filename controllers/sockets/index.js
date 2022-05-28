6c8e5dc575b94f0ef4e28da8288bf3caf6c5690f
DESKTOP-0781089\Loc <ducloc0009@gmail.com>
2022-05-28 14:49:47 +0700

    test socketIO

---
+++ controllers/sockets/index.js
@@ -0,0 +1,3 @@
+import playerJoinHandle from "./joinHandle.js";
+
+export { playerJoinHandle };
--- server.js
+++ server.js
@@ -1,34 +1,50 @@
-import express from 'express';
-import logger from 'morgan';
-import 'dotenv/config';
-import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import serverRouter from './routes/server.router.js';
+import express from "express";
+import logger from "morgan";
+import "dotenv/config";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import serverRouter from "./routes/server.route.js";
+import http from "http";
+import { Server } from "socket.io";
+import { playerJoinHandle } from "./controllers/sockets/index.js";
 
 const app = express();
-
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 const port = process.env.PORT_SERVER || 3000;
-
-app.use(cors(
-    {
-        methods: 'GET, PATCH, POST, DELETE'
-    }
-));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(logger('dev'));
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/server", serverRouter);
 
-app.use('/server', serverRouter);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+io.on("connection", (socket) => {
+  console.log(`user ${socket.id} connect to socket server`);
+  playerJoinHandle(io, socket);
+
+  // Player disconnect
+  socket.on("disconnect", (reason) => {
+    console.log(reason);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`socket is listening on: ${port}`);
+});
